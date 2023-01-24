ddf8ab37c46ac928afa996a3bafd748807e7ef53
gkmohanc79 <gkmohanc79@gmail.com>
2023-01-24 11:28:46 -0500

    Java demo

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number1");
+int a = sc.nextInt();
+        System.out.println("Enter the number2");
+        int b = sc.nextInt();
+            for (int j=1; j<=10; j++){
+
+            System.out.printf("%d * %d = %d", a, j, (a*j)).println();
+
+        } for ( int k=1; k<=10; k++)
+            System.out.printf("%d * %d = %d", b, k, (b*k)).println();
+
+
+//        System.out.println("a*b= "+ (a+b+c));
+    }
+}
